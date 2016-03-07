7309162698c5fa8709e02de9b7f268924fa4530f
Robert Hoelzl <robert.hoelzl@posteo.de>
2016-03-07 10:18:20 +0100

    repeating creration multiple times works now, even if result image was already imported into vagrant some time before.

--- step3_run_on_host.cmd
+++ step3_run_on_host.cmd
@@ -1,4 +1,4 @@
 vagrant package --output vanilla-win7-32bit.box --vagrantfile Vagrantfile
-vagrant box add --name vanilla-win7-32bit vanilla-win7-32bit.box
+vagrant box add --force --name vanilla-win7-32bit vanilla-win7-32bit.box
 vagrant destroy --force
 del vanilla-win7-32bit.box
