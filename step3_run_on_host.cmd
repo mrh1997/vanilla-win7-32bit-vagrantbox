a970306a0a71ddc4eea2638ae0c597f245b5276e
Robert Hoelzl <robert.hoelzl@posteo.de>
2016-05-12 20:47:49 +0200

    Not automaticially importing vanilla-win7-32bit.box any more in step3.
    The box should be importet implicitily (on the first usage) from atlas, since then the version and atlas-account name are added (it is named "mrh1997/vanilla-win7-32bit" instead of "vanilla-win7-32bit" then)

--- step3_run_on_host.cmd
+++ step3_run_on_host.cmd
@@ -1,4 +1,3 @@
 vagrant halt
 vagrant package --output vanilla-win7-32bit.box --vagrantfile Vagrantfile
-vagrant box add --force --name vanilla-win7-32bit vanilla-win7-32bit.box
 vagrant destroy --force
